cdc895b4d7d67c2acc2a6861de6dbedb0c02b482
Paul Jackson <paul.jackson.uk@gmail.com>
2025-08-16 19:58:27 +0100

    Improved position evaluation, stopped search going too deep unless low material count

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,101 +5,23 @@ using System.Runtime.InteropServices;
 using ChessChallenge.API;
 using System.Collections;
 using System;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 public class MyBot : IChessBot
 {
-    int [] pawn_square_scores = new int[] {
-        0, 0, 0, 0, 0, 0, 0, 0,
-        20, 20, 10, 0, 0, 20, 20, 20,
-        10, 10, 10, 20, 20, 20, 20, 10,
-        5 ,5 ,10 ,35 ,35 ,10 ,5 ,5 ,
-        0 ,0 ,0 ,20 ,20 ,0 ,0 ,0 ,
-        -5,-5,-10,-5,-5,-10,-5,-5,
-        -10,-10,-20,-30,-30,-20,-10,-10,
-        0, 0, 0, 0, 0, 0, 0, 0
-    };
-
-    int[] knight_square_scores = new int[] {
-        -100, -50, -30, -30, -30, -30, -30, -100,
-        -50, -50, -20, -20, -20, -20, -50, -50,
-        -30, -20, 10, 10, 10, 10, -20, -30,
-        -30, -20, 10, 15, 15, 10, -20, -30,
-        -30, -10, 10, 15, 15, 10, 10, -30,
-        -30, 0, 10, 10, 10, 10, 0, -30,
-        -50, -50, -20, -20, -20, -20, -50, -50,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0
-    };
-
-    int[] rook_square_scores = new int[] {
-        0, 0, 0, 0, 0, 0, 0, 0,
-        5, 10, 10, 10, 10, 10, 10, 5,
-        -5, 0, 0, 0, 0, 0, -5, -5,
-        -5, -5, -5, -5, -5, -5, -5, -5,
-        -5, -5, -5, -5, -5, -5, -5, -5,
-        -10,-10,-10,-10,-10,-10,-10,-10,
-        -20,-20,-20,-20,-20,-20,-20,-20,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0
-    };
-
-    int[] bishop_square_scores = new int[] {
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        10, 10, 10, 10, 10, 10, 10, 10,
-        -5, -5, -5, -5, -5, -5, -5, -5,
-        -5, -5, -5, -5, -5, -5, -5, -5,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0
-    };
-
-    int[] queen_square_scores = new int[] {
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        10, 10, 10, 10, 10, 10, 10, 10,
-        -5, -5, -5, -5, -5, -5, -5, -5,
-        -5, -5, -5, -5, -5, -5, -5, -5,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0
-    }; 
-
-    int[] king_square_scores = new int[] {
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        5 ,5 ,5 ,5 ,5 ,5 ,5 ,5 ,
-        10,10,10,10,10,10,10,10,
-        20,20,20,20,20,20,20,20,
-        30,30,30,30,30,30,30,30,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0
-    };
-    int[] none_square_scores = new int[] {
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        0 ,0 ,0 ,0 ,0 ,0 ,0 ,0,
-        1, 1, 1, 1, 1, 1, 1, 1
-    };
-    int[][] square_scores;
-
-    public MyBot()
-    {
-        square_scores = new int[][]
-        {
-            none_square_scores,
-            pawn_square_scores,
-            knight_square_scores,
-            bishop_square_scores,
-            rook_square_scores,
-            queen_square_scores,
-            king_square_scores
-        };
-    }
+    private uint numLegalMovesCalls = 0;
     public static int evaluationCount = 0;
     private Dictionary<ulong,int> evaluated_positions = new Dictionary<ulong, int>();
     private int[] pieceValues = { 0, 100, 300, 300, 500, 900, 20000 }; // none, Pawn, Knight, Bishop, Rook, Queen, King
+
+    private int GetEdgeDistance(int squareIndex)
+    {
+        int row = squareIndex / 8;
+        int col = squareIndex % 8;
+
+        // Calculate the distance to the nearest edge
+        int distanceToEdge = Math.Min(Math.Min(row, 7 - row), Math.Min(col, 7 - col));
+        return distanceToEdge;
+    }
     private int Evaluate(Board board, bool isWhite)
     {
         evaluationCount++;
@@ -111,98 +33,120 @@ public class MyBot : IChessBot
         {
             // add up material value of pieces
             int value = pieceValues[(int)pl.TypeOfPieceInList];
-            if (pl.IsWhitePieceList != isWhite) value = -value;
-            score += value * pl.Count;
-
-            // Add a position score
-            var square_scores = this.square_scores[(int)pl.TypeOfPieceInList];
+            if (pl.TypeOfPieceInList is not (PieceType.Queen or PieceType.King or PieceType.Rook))
             {
-                // Add positional score for peices
                 for (int i = 0; i < pl.Count; i++)
                 {
-                    Piece piece = pl.GetPiece(i);
-                    int squareIndex = piece.Square.Index;
-                    int sq = (piece.IsWhite) ? squareIndex : 63 - squareIndex;
-                    if (isWhite == piece.IsWhite)
-                    {
-                        score += square_scores[sq];
-                    }
-                    else
-                    {
-                        score -= square_scores[sq];
-                    }
+                    // Add positional score for pieces
+                    value += 8 * GetEdgeDistance(pl.GetPiece(i).Square.Index);
                 }
             }
 
+            // Add or subtract peice value based on colour
+            if (pl.IsWhitePieceList != isWhite) value = -value;
+            score += value * pl.Count;
+
         }
 
-        
-        if (isWhite && ((board.WhitePiecesBitboard & (1UL << 28)) != 0))
-        {
-            // Example of a positional bonus for White's King being on e1
-            score += 50; // Arbitrary bonus for pawn centre 
-        }
-        if (isWhite && ((board.WhitePiecesBitboard & (1UL << 29)) != 0))
-        {
-            // Example of a positional bonus for White's King being on e1
-            score += 50; // Arbitrary bonus for pawn centre 
-        }
 
         return score;
     }
 
+    
     private (int, Move) get_best_move(Board board, bool isWhite, int depth, int alpha, int beta)
     {
 
         Move best_move_this_level = Move.NullMove;
         int best_score_this_level = (isWhite == board.IsWhiteToMove) ? int.MinValue : int.MaxValue;
 
-        depth--;
-        Move[] moves = board.GetLegalMoves(depth < 0);
-
-        foreach (Move move in moves)
+        if (board.IsInCheckmate())
         {
-            int score;
-            board.MakeMove(move);
-            // Have already evaluated this position?
-            if (evaluated_positions.TryGetValue(board.ZobristKey, out int cached_score))
-            {
-                score = cached_score;
-            }
-            else
-            {
-                (score, _) = get_best_move(board, isWhite, depth, alpha, beta);
-                evaluated_positions.TryAdd(board.ZobristKey, score);
-            }
-            board.UndoMove(move);
-
             if (isWhite == board.IsWhiteToMove)
             {
-                // our move
-                if (score > best_score_this_level)
-                {
-                    best_score_this_level = score;
-                    best_move_this_level = move;
-                    alpha = Math.Max(alpha, score);
-                    if (beta <= alpha)
-                    {
-                        // Beta cut-off
-                        return (best_score_this_level, best_move_this_level);
-                    }
-                }
+                // We are in checkmate, so we lose
+                return (int.MinValue, Move.NullMove);
             }
             else
             {
-                // opponent's move
-                if (score < best_score_this_level)
+                // They are in checkmate, so we win
+                return (int.MaxValue, Move.NullMove);
+            }
+        }
+        else if (--depth > -12)
+        {
+            Move[] moves = board.GetLegalMoves(depth < 0);
+            if (moves.Length > 1)
+            {
+                Array.Sort(moves, (a, b) =>
                 {
-                    best_score_this_level = score;
-                    best_move_this_level = move;
-                    beta = Math.Min(beta, score);
-                    if (beta <= alpha)
+                    if (a.IsCapture && !b.IsCapture) return -1;
+                    if (!a.IsCapture && b.IsCapture) return 1;
+                    return 0;
+                });
+            }
+
+            foreach (Move move in moves)
+            {
+                int score = 0;
+                board.MakeMove(move);
+                try
+                {
+                    if (board.IsDraw())
                     {
-                        // Alpha cut-off
-                        return (best_score_this_level, best_move_this_level);
+                        score = 0;
+                    }
+                    else
+                    {
+                        // Have already evaluated this position?
+                        if (evaluated_positions.TryGetValue(board.ZobristKey, out int cached_score))
+                        {
+                            score = cached_score;
+                        }
+                        else
+                        {
+                            (score, _) = get_best_move(board, isWhite, depth, alpha, beta);
+                            evaluated_positions.TryAdd(board.ZobristKey, score);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    System.Console.WriteLine("Exception: " + e.Message);
+                    System.Console.WriteLine(board.CreateDiagram());
+                }
+                finally
+                {
+                    board.UndoMove(move);
+                }
+
+                if (isWhite == board.IsWhiteToMove)
+                {
+                    // our move
+                    if (score > best_score_this_level)
+                    {
+                        best_score_this_level = score;
+                        best_move_this_level = move;
+                        alpha = Math.Max(alpha, score);
+                        if (beta <= alpha)
+                        {
+                            // Beta cut-off
+                            return (best_score_this_level, best_move_this_level);
+                        }
+                    }
+                }
+                else
+                {
+                    // opponent's move
+                    if (score < best_score_this_level)
+                    {
+                        best_score_this_level = score;
+                        best_move_this_level = move;
+                        beta = Math.Min(beta, score);
+                        if (beta <= alpha)
+                        {
+                            // Alpha cut-off
+                            return (best_score_this_level, best_move_this_level);
+                        }
                     }
                 }
             }
@@ -223,9 +167,28 @@ public class MyBot : IChessBot
 
     public Move Think(Board board, Timer timer)
     {
+        System.Console.WriteLine("Thinking...");
+        if (board.IsInCheckmate() || board.IsDraw())
+        {
+            System.Console.WriteLine("Checkmate or draw detected.");
+            //return Move.NullMove; // No valid moves if in checkmate or draw
+        }
         evaluationCount = 0;
         evaluated_positions.Clear();
-        int depth = 4;
+        var all_bb = board.AllPiecesBitboard;
+
+        int depth = 2;
+
+        switch (BitboardHelper.GetNumberOfSetBits(all_bb))
+        {
+            case < 6: depth = 12; break;
+            case < 8: depth = 10; break;
+            case < 10: depth = 8; break;
+            case < 14: depth = 6; break;
+            case < 18: depth = 4; break;
+            default: depth = 2; break;
+        }
+
         bool isWhite = board.IsWhiteToMove;
         Move best_move = Move.NullMove;
         int best_score = isWhite ? int.MinValue : int.MaxValue;
@@ -233,13 +196,12 @@ public class MyBot : IChessBot
         int beta = int.MaxValue;
         (best_score, best_move) = get_best_move(board, isWhite, depth, alpha, beta);
         System.Console.Write($"Best move score {best_score}"  );
-        System.Console.Write($"Total evaluations: {evaluationCount} ");
-        Move[] history = board.GameMoveHistory.ToArray();
-        foreach (Move m in history)
+
+        if (!board.GetLegalMoves().Contains(best_move))
         {
-            System.Console.Write($" {m.ToString()}");
+            throw new Exception("ERROR: I messed smt up");
         }
-        System.Console.WriteLine("");
+        System.Console.WriteLine(" - Best move: " + best_move.ToString() + " - Evaluation count: " + evaluationCount);
         return best_move;
     }
 }
